928c53c3625857e01f9b35e06296cd6f4985274f
HyokeunLee <hyok7575@gmail.com>
2017-01-04 08:12:30 +0900

    routes/index , views/index 수정

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'ddd' });
 });
 
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'dada' });
+});
+
 module.exports = router;
